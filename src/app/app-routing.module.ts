dcc7e30584671d1731972bbe313bcf7a23b6a32a
Lokesh Sutraye <lsutraye@BLR.ALLEGISINDIA.COM>
2021-10-16 17:12:57 +0530

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './admin/books/books.component';
+import { UsersComponent } from './admin/users/users.component';
+import { ShopbookComponent } from './shopbook/shopbook.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin/users', component: UsersComponent },
+  { path: 'admin/books', component: BooksComponent },
+  { path: 'shop', component: ShopbookComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
